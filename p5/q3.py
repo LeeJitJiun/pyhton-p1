f5598d9aaae7dfed84588718bc5eaa4954e7dc48
LeeJitJiun <leejj-wm24@student.tarc.edu.my>
2025-07-23 19:52:05 +0800

    add on q5, q6

--- p5/q3.py
+++ p5/q3.py
@@ -1,15 +1,23 @@
 a_i = 0.05
 m_i = a_i / 12
 
-base = float(input("Enter the monthly saving amount: "))
+base = 100
+total = 0
+repetition = 10
+month = ""
 
-total = base * (1 + m_i)
-total = (base + total) * (1 + m_i)
-total = (base + total) * (1 + m_i)
-total = (base + total) * (1 + m_i)
-total = (base + total) * (1 + m_i)
-total = (base + total) * (1 + m_i)
+print("{:10s}{:10s}".format("Month","Total"))
 
-print("After the sixth month, the account value is" , total)
-print("After the sixth month, the account value is" , (round(total,2)))  
-
+for count in range(repetition):
+    total = (base + total) + (1 + m_i)
+    
+    match (count + 1) % 10: 
+        case 1:
+            month = str(count + 1) + "st"
+        case 2:
+            month = str(count + 1) + "nd"
+        case 3:
+            month = str(count + 1) + "rd"
+        case _:
+            month = str(count + 1) + "th" 
+    print("{:10s} RM {:<10.2f}".format(month,total))
